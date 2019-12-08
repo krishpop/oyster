4f73e9bdc479a4554fd60c07b60746393912ea73
Krishnan Srinivasan <krishnan1994@gmail.com>
2019-12-07 17:27:19 -0800

    added import failure checks

--- rlkit/envs/__init__.py
+++ rlkit/envs/__init__.py
@@ -23,4 +23,7 @@ def register_env(name):
 for file in os.listdir(os.path.dirname(__file__)):
     if file.endswith('.py') and not file.startswith('_'):
         module = file[:file.find('.py')]
-        importlib.import_module('rlkit.envs.' + module)
+        try:
+            importlib.import_module('rlkit.envs.' + module)
+        except Exception as e:
+            print('skipping import of {} because got error {}'.format(module, e))
